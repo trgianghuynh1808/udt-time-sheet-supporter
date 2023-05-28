44edb28e7737c8f32e39f6c5383e936206e026b3
Giang Huynh <trgianghuynhdev1808@gmail.com>
2023-05-28 09:53:34 +0700

    imp: write sheet for sheet service

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '1VGWsZ7xiFYmpsPbCrm00gwsjmxcuzbZwZJT3CbTr54I'
-RANGE_NAME = 'sheet1!A1:D2'
+RANGE_NAME = 'timesheet!A1:F2'
--- sheet.py
+++ sheet.py
@@ -1,13 +1,16 @@
 from config import *
 from googleapiclient.discovery import build
 
-def getSheetValues(credentials):
-    service = build('sheets', 'v4', credentials=credentials)
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    sheetValues = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+class SheetService:
+    def __init__(self, credentials):
+        service = build('sheets', 'v4', credentials=credentials)
+        sheet = service.spreadsheets()
+        sheet_values = sheet.values()
+        self.sheet_values = sheet_values
 
-    return sheetValues
+    def readSheet(self):
+        result = self.sheet_values.get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+        return result.get('values', [])
 
-def readSheet(sheetValues):
-    return sheetValues.get('values', [])
+    def writeSheet(self, range, body_data):
+        return self.sheet_values.update(spreadsheetId=SPREADSHEET_ID, range=range, valueInputOption="USER_ENTERED", body={"values": body_data}).execute()
